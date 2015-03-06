3a173bb746251810825d8c381c351226f3a56600
Matthew Bunday <zenceph@gmail.com>
2015-03-06 11:51:39 -0500

    Board interface

--- board.rb
+++ board.rb
@@ -1,6 +1,16 @@
 class Board
   def initialize(board_str = nil)
+    @board = {}
+  end
 
+  def set!(row, col, piece)
+    piece.row = row
+    piece.col = col
+    @board[[row, col]] = piece
+  end
+
+  def get(row, col)
+    @board[[row, col]]
   end
 
   def setup_standard_chess!
