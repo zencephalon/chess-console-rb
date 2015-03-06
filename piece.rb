dc148345c14de1abe2d365429ce400fa82ebff86
Matthew Bunday <zenceph@gmail.com>
2015-03-06 11:38:58 -0500

    Add pawn, abstract to_s

--- piece.rb
+++ piece.rb
@@ -2,4 +2,8 @@ class Piece
   def initialize(row, col, color)
     @row, @col, @color = row, col, color
   end
+
+  def to_s
+    @color == :white ? self.class::SYMBOL[0] : self.class::SYMBOL[1]
+  end
 end
