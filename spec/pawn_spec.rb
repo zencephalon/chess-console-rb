c588aa7cbaab26c5334bc88526935acfe93c761c
Matthew Bunday <zenceph@gmail.com>
2015-03-06 15:08:41 -0500

    New pawn spec for double move

--- spec/pawn_spec.rb
+++ spec/pawn_spec.rb
@@ -1,13 +1,19 @@
 require 'board'
 
 describe Pawn do
-  it "should have the ability to double move on first move" do
+  let(:board) { 
     board = Board.new(<<-chess)
 ..
 ..
 Pp
     chess
+  }
 
+  it "can double move on first move" do
     expect(board.get(0, 0).moves).to eq([[1, 0], [2, 0]])
+  end
+
+  it "can't double move after first move" do
+
   end
 end
