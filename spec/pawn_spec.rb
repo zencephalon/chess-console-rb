b37cb1772dead776ada1da3f10b218b8d9bcc405
Matthew Bunday <zenceph@gmail.com>
2015-03-06 23:39:49 -0500

    Spec for pawn attacks

--- spec/pawn_spec.rb
+++ spec/pawn_spec.rb
@@ -2,7 +2,7 @@ require 'board'
 
 describe Pawn do
   let(:board) { 
-    board = Board.new(<<-chess)
+    Board.new(<<-chess)
 ..
 ..
 Pp
@@ -16,5 +16,16 @@ Pp
   it "can't double move after first move" do
     board.move!([0, 0], [1, 0])
     expect(board.get([1, 0]).moves).to eq([[1,0]])
+  end
+
+  let(:attack_board) {
+    Board.new(<<-chess)
+p.p
+.P.
+    chess
+  }
+
+  it "can attack both enemies" do
+    expect(attack_board.get([0, 1]).moves).to eq([[1, 0], [2, 0], [1, 1], [1, -1]])
   end
 end
