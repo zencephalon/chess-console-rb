8ccaba809015ada870a23fba507178748be21271
Matthew Bunday <zenceph@gmail.com>
2015-06-19 11:45:41 -0400

    Add LineMover to queen

--- lib/queen.rb
+++ lib/queen.rb
@@ -1,3 +1,7 @@
+require_relative 'line_mover'
+
 class Queen < Piece
+  include LineMover
   SYMBOL = ['♕', '♛']
+  DIRECTIONS = [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]]
 end
