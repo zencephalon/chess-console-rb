6d7ea369e6f33b868412454f32514dac944256f8
Matthew Bunday <zenceph@gmail.com>
2015-06-19 12:14:31 -0400

    Implement knight

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,3 +1,9 @@
 class Knight < Piece
   SYMBOL = ['♘', '♞']
+
+  def moves
+    m = [[2, 1], [2, -1], [-2, 1], [-2, -1], [1, 2], [1, -2], [-1, 2], [-1, -2]]
+    m = bounds_check(m)
+    m.reject {|move| piece = @board.get(rel(move)); piece && ! enemy?(piece)}
+  end
 end
