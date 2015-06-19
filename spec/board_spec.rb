099a69793aeb4402c753bbbbfc6b0d83d089e54a
Matthew Bunday <zenceph@gmail.com>
2015-06-19 08:39:09 -0400

    Spec for board.to_s

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -9,4 +9,16 @@ Pp
 
     expect(board.get([0, 0])).to be_a(Pawn)
   end
+
+  it "should have a working to_s" do
+    board = Board.new(<<-chess)
+..
+Pp
+    chess
+
+    expect(board.to_s).to eq(<<-chess.chomp)
+  
+♙♟
+    chess
+  end
 end
