229b3c5e85a471954807c6153a8a13ad4a4ca450
Matthew Bunday <zenceph@gmail.com>
2015-06-19 10:44:40 -0400

    Refactor pawn moves

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -4,13 +4,15 @@ class Pawn < Piece
   def moves
     dir = (@color ? 1 : -1)
     single, double = [dir, 0], [dir * 2, 0]
-    m = [single]
-    m << double unless @moved
-    m.delete(double) if @board.get(rel(double))
-    if @board.get(rel(single))
-      m.delete(single)
-      m.delete(double)
-    end
+
+    m = if @board.get(rel(single))
+          []
+        elsif @moved || @board.get(rel(double))
+          [single]
+        else
+          [single, double]
+        end
+
     [[dir, 1], [dir, -1]].each do |attack|
       m << attack if enemy?(@board.get(rel(attack)))
     end
